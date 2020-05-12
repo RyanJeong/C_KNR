4f7956cf9f87f5eca040cc48d2fe2c23a36f051c
RyanJeong <wat6988@gmail.com>
2020-05-12 17:55:00 +0900

    Updated on May 12 2020

--- chapter2/exercise2-05/main.c
+++ chapter2/exercise2-05/main.c
@@ -1,68 +1,45 @@
 /*
- *	Write the function any(s1,s2), which returns the first location in the string s1 where any character from the string s2 occurs,
- *	or -1 if s1 contains no characters from s2.
- *	(The standard library function strpbrk does the same job but returns a pointer to the location.)
+ *  Write the function any(s1,s2), which returns the first location in the string s1 where any character from the string s2 occurs,
+ *  or -1 if s1 contains no characters from s2.
+ *  (The standard library function strpbrk does the same job but returns a pointer to the location.)
  */
 #include <stdio.h>
-#define MAX 100
+
+enum {
+    MAX = 100
+};
 
 void any(char *, char *);
 
 int main(void)
 {
-	int	c, i, limit;
-	char	s1[MAX], s2[MAX];
+    char    s1[MAX], s2[MAX];
 
-	limit = (MAX - 1);
-	puts("Input s1");
-	for (i = 0; ((c = getchar()) != EOF); ++i) {
-		if (i == limit) {
-			while (getchar() != '\n') {
-				;
-			}
-			break;
-		}
-		if (c == '\n') {
-			break;
-		}
-		s1[i] = c;
-	}
-	s1[i] = '\0';
-	puts("Input s2");
-	for (i = 0; ((c = getchar()) != EOF); ++i) {
-		if (i == limit) {
-			while (getchar() != '\n') {
-				;
-			}
-			break;
-		}
-		if (c == '\n') {
-			break;
-		}
-		s2[i] = c;
-	}
-	s2[i] = '\0';
-	printf("s1: %s, s2: %s\n", s1, s2);
-	any(s1, s2);
+    puts("Input s1");
+    scanf("%s", s1);
+    getchar();
+    puts("Input s2");
+    scanf("%s", s2);
+    getchar();
+    printf("s1: %s, s2: %s\n", s1, s2);
+    any(s1, s2);
 
-	return 0;
+    return 0;
 }
 
 void any(char *s1, char *s2)
 {
-	int	c, i;
-	char	*p_s1;
+    char    *p_s1;
 
-	while ((c = *s2++)) {
-		p_s1 = s1;
-		for (p_s1 = s1; *p_s1; ++p_s1) {
-			if (*p_s1 == c) {
-				break;
-			}
-		}
-		i = (!*p_s1) ? (-1) : (p_s1 - s1);
-		printf("%c\'s location: %d\n", c, i);
-	}
+    while (*s2) {
+        p_s1 = s1;
+        for (p_s1 = s1; *p_s1; ++p_s1) {
+            if (*p_s1 == *s2) {
+                break;
+            }
+        }
+        printf("%c\'s location: %d\n", *s2++, (!*p_s1) ? (-1) : (int) (p_s1 - s1));
+    }
 
-	return;
+    return;
 }
--- chapter2/exercise2-10/main.c
+++ chapter2/exercise2-10/main.c
@@ -1,24 +1,24 @@
 /*
- *	Rewrite the function lower, which converts upper case letters to lower case, with a conditional expression instead of if-else .
+ *  Rewrite the function lower, which converts upper case letters to lower case, with a conditional expression instead of if-else .
  */
 #include <stdio.h>
 
 int lower(int);
 
-int main()
+int main(void)
 {
-	int c;
+    int c;
 
-	puts("Input only alphabets");
-	while ((c = getchar()) != EOF) {
-		putchar(lower(c));
-	}
+    puts("Input only alphabets");
+    while ((c = getchar()) != EOF) {
+        putchar(lower(c));
+    }
 
-	return 0;
+    return 0;
 }
 
 int lower(int c)
 {
 
-	return (((c >= 'A') && (c <= 'Z')) ? (c + ('a' - 'A')) : c);
+    return (((c >= 'A') && (c <= 'Z')) ? (c + ('a' - 'A')) : c);
 }
