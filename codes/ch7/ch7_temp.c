feb1afdf7154be3e0f9e85d9ed52ba682acbf83c
RyanJeong <wat6988@gmail.com>
2021-05-29 22:19:38 +0900

    Updated on May 29, 2021

--- codes/ch7/ch7_temp.c
+++ codes/ch7/ch7_temp.c
@@ -53,6 +53,17 @@ void f()
 FILE *fp;
 FILE *fopen(char *name, char *mode);
 int getc(FILE *fp);
+int putc(int c, FILE *fp);
+
+#define getchar()  getc(stdin)
+#define putchar(c) putc((c), stdout)
+
+int fscanf(FILE *fp, char *format, ...);
+int fprintf(FILE *fp, char *format, ...);
+
+int ferror(FILE *fp);
+int feof(FILE *fp);
+
 
 void b()
 {
--- codes/test.c
+++ codes/test.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h> /* exit() function declared here */
+#include <ctype.h>
 
-int main(void)
+void filecopy(FILE *, FILE *);
+
+int main(int argc, char *argv[])
 {
-    int a, b, c;
+    FILE *src, *dst;
 
-    /* > 13 23 41 */
-    printf("%d\n", scanf("%d %d %d", &a, &b, &c));
+    src = fopen("source.txt", "r");
+    dst = fopen("destination.txt", "w");
+    /* check error */
+    if (src == NULL || dst == NULL) {
+        printf("Unable to open file\n");
+
+        exit(1);
+    }
+    filecopy(src, dst);
+    fclose(src);
+    fclose(dst);
 
     return 0;
+}
+
+/* filecopy: copy file ifp to file ofp */
+void filecopy(FILE *ifp, FILE *ofp)
+{
+    int c;
+
+    while ((c = getc(ifp)) != EOF) {
+        /* The putc function normally return a copy of the byte that is has written - as confirmation of success.
+           If there is an error it returns EOF instead. */
+        if (putc(c, ofp) == EOF) {
+            printf("Error in writing to file\n");
+
+            exit(1);
+        }
+    }
+    if (feof(ifp)) {
+        printf("End of file\n");
+    }
+    if (ferror(ifp)) {
+        printf("Error in reading from file\n");
+        
+        exit(1);
+    }
+
+    return;
 }
