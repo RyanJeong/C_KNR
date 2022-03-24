0c8379ccfcb1aafb21c544eb9523a13002d3da7a
Ryan M. Jeong <wat6988@gmail.com>
2022-03-24 19:17:40 +0900

    Update 07.c

--- codes/ch3/07.c
+++ codes/ch3/07.c
@@ -3,15 +3,16 @@ void itoa(int n, char s[])
 {
   int i, sign;
 
-  if ((sign = n) < 0) /* record sign   */
-    n = (-n);     /* make n positive */
+  if ((sign = n) < 0) /* record sign */
+    n = (-n); /* make n positive */
   i = 0;
   do {
     s[i++] = (n % 10) + '0'; /* get next digit */
-  } while ((n /= 10) > 0);   /* delete it   */
+  } while ((n /= 10) > 0); /* delete it */
   if (sign < 0)
     s[i++] = '-';
   s[i] = '\0';
+
   /* not a standard library */
   reverse(s);
 }
