8a47bafc450fa832c2c71cd9cd4ea42b1520c36b
RyanJeong <wat6988@gmail.com>
2022-03-25 04:40:42 +0900

    Updated on Mar 25, 2022

--- codes/ch2/13.c
+++ codes/ch2/13.c
@@ -4,7 +4,7 @@ void strcat(char s[], char t[])
   int i, j;
 
   i = j = 0;
-  while (s[i] != '\0') /*find end of s*/
+  while (s[i] != '\0') /* find end of s */
     ++i;
-  while ((s[i++] = t[j++]) != '\0') {} /*copy t*/
+  while ((s[i++] = t[j++]) != '\0') {} /* copy t */
 }
--- codes/ch2/15.c
+++ codes/ch2/15.c
@@ -9,10 +9,10 @@ int main(void)
   printf("x & 077 = %d\n", x & 077); /* mask operation */
 
   /* The result is 1111 1111 */
-  printf("x | 07 = %d\n", x | 07);  /* set operation  */
+  printf("x | 07 = %d\n", x | 07); /* set operation */
 
   /* The result is 1100 0000 */
-  printf("x & ~077 = %d\n", x & ~077);  /* better than 0x300  */
+  printf("x & ~077 = %d\n", x & ~077); /* better than 0x300 */
   /* 
    * 0x300, which assumes that x is a 8-bit quantity
    * ~077 is independent of word length
